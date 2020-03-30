1e03ca57ccdf0a022b7041127794c9523422f34c
BoringMind <super.d3280@ya.ru>
2020-03-30 14:21:34 +0300

    Added lists app. Added tests for homepage url

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from superlists.lists import views as listviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', listviews.home_page, name="home"),
 ]
