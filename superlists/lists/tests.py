67f68d03a4914a5c08f37f5f9d0d932e9fc0693f
BoringMind <super.d3280@ya.ru>
2020-04-01 16:40:20 +0300

    Added to-do items saving to db. Refactor of the unit tests: remove unnecessary import

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
-from django.urls import resolve
-from django .http import HttpRequest
-from superlists.lists.views import home_page
+
+from superlists.lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -14,3 +13,25 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+    """Test model for to-do items."""
+
+    def save_item(self, item_text):
+        item = Item()
+        item.text = item_text
+        item.save()
+        return item
+    
+    def test_saving_and_retrieving_items(self):
+        first_item = self.save_item('The first (ever) listed item')
+        second_item = self.save_item('Item the second')
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item, first_item)
+        self.assertEqual(second_saved_item, second_item)
--- superlists/tests/functional_tests.py
+++ superlists/tests/functional_tests.py
@@ -1,7 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import unittest
 import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
